f15cee039ff69cce9aecddb432d13d3f01ada78b
Jahn Tore Hansen <jahntore@gmail.com>
2020-06-10 23:23:54 +0200

    vue.config.js ting

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-    publicPath: ''
+    publicPath: '',
+    devServer: {
+        host: '0.0.0.0',
+        port: 8081, // CHANGE YOUR PORT HERE!
+        proxy: {
+            '^/Plone': {
+              target: 'http://localhost:8080',
+              ws: true,
+              changeOrigin: true,
+              //pathRewrite: {'^/api': '/Plone'},
+            }
+        }
+      }
 }
